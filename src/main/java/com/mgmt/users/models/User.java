40b206f11cb1d19f12d5b865fc2860ecf7791259
srinivasu619 <srinivasu.618@gmail.com>
2020-06-09 21:59:53 +0530

    Implemeneted User Model

---
+++ src/main/java/com/mgmt/users/models/User.java
@@ -0,0 +1,49 @@
+package com.mgmt.users.models;
+
+public class User {
+    private String name;
+    private Integer age;
+    private String email;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the age
+     */
+    public Integer getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
